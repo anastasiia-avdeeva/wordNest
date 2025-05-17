4783fc1bf0dc16270c01b7ce6cbb3be90f7b579b
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-17 03:37:18 +0300

    create small words list

---
+++ src/constants/constants.js
@@ -0,0 +1,30 @@
+export const words = [
+  {
+    id: 1,
+    word: "apple",
+    transcription: "['æp.əl]",
+    translation: "яблоко",
+    theme: "Еда",
+  },
+  {
+    id: 2,
+    word: "book",
+    transcription: "[bʊk]",
+    translation: "книга",
+    theme: "Учёба",
+  },
+  {
+    id: 3,
+    word: "dog",
+    transcription: "[dɒg]",
+    translation: "собака",
+    theme: "Животные",
+  },
+  {
+    id: 4,
+    word: "sun",
+    transcription: "[sʌn]",
+    translation: "солнце",
+    theme: "Природа",
+  },
+];
