1840443de55ea7a8b36ddca18e083cfedc11a6b7
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-22 01:09:07 +0300

    write function to sort words array by word and use it in WordTable component

--- src/components/WordTable/WordTable.jsx
+++ src/components/WordTable/WordTable.jsx
@@ -2,17 +2,19 @@ import { wordTableHeader, words } from "../../constants/constants";
 import { HeaderTableRow } from "../HeaderTableRow/HeaderTableRow";
 import { WordTableRow } from "../WordTableRow/WordTableRow";
 import { WordEditableRow } from "../WordEditableRow/WordEditableRow";
+import { sortByName } from "../../utils/stringUtils";
 import styles from "./WordTable.module.css";
 
 export const WordTable = () => {
   const onEdit = 1;
+  const sortedWords = sortByName(words);
   return (
     <table className={styles.table}>
       <thead className={styles.table__header}>
         <HeaderTableRow array={wordTableHeader} />
       </thead>
       <tbody className={styles.table__body}>
-        {words.map((word) =>
+        {sortedWords.map((word) =>
           word.id === onEdit ? (
             <WordEditableRow key={word.id} word={word} />
           ) : (
--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -2,3 +2,9 @@ export function capitalizeStr(str) {
   if (!str) return "";
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
+
+export function sortByName(objArr) {
+  return objArr.toSorted((a, b) =>
+    a.word.localeCompare(b.word, "ru", { sensitivity: "base" })
+  );
+}
