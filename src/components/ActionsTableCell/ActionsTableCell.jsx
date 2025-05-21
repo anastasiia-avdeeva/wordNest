9a6194d027b689b956c14f0c3e027e8500651862
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2025-05-21 21:56:52 +0300

    make ActionTableCell more flexible

--- src/components/ActionsTableCell/ActionsTableCell.jsx
+++ src/components/ActionsTableCell/ActionsTableCell.jsx
@@ -1,11 +1,11 @@
 import { TextButton } from "../TextButton/TextButton";
 import styles from "./ActionsTableCell.module.css";
 
-export const ActionsTableCell = () => {
+export const ActionsTableCell = ({ btnText1, btnText2 }) => {
   return (
     <div className={styles.table__buttons}>
-      <TextButton variant="edit-button">Редактировать</TextButton>
-      <TextButton variant="del-button">Удалить</TextButton>
+      <TextButton variant="edit-button">{btnText1}</TextButton>
+      <TextButton variant="del-button">{btnText2}</TextButton>
     </div>
   );
 };
